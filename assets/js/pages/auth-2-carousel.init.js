00f138df35afc1bf221ec0a0fa6ef625061994f9
ozzaynetworks <ozzaynetworks@gmail.com>
2022-02-18 13:26:03 +0300

    kelvin:authentication pages

--- assets/js/pages/auth-2-carousel.init.js
+++ assets/js/pages/auth-2-carousel.init.js
@@ -1,1 +1,10 @@
-$("#auth-review-carousel").owlCarousel({items:1,loop:!1,margin:16,nav:!1,dots:!0});
+$("#auth-review-carousel").owlCarousel({
+    items: 1,
+    margin: 16,
+    nav: !1,
+    dots: !0,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: true,
+    loop: true,
+});
